adbd0c2c5c92d9da1690fd68587b060cb15a3470
CasTolax <cantuncer32@gmail.com>
2025-07-01 11:53:45 +0300

    Add files via upload

---
+++ operatörler/src/operatörler/operatortest.java
@@ -0,0 +1,22 @@
+package operatörler;
+
+public class operatortest {
+
+	public static void main(String[] args) {
+		int ders1 = 34;
+		int ders2 = 67;
+		int ders3 = 87;
+		int ders4 = 54;
+		
+		
+		//System.out.println((ders1 + ders2 + ders3 + ders4) / 3); // ders1,ders2,ders3,ders4 toplar ve ortalaması alınır
+		
+		String isim = "can";
+		String soyisim = "tuncer";
+		
+		System.out.println("isminiz:" + isim + " " + "soyisminiz:"+ " " + soyisim );
+		System.out.println("4 dersin ortalaması:" + (ders1 + ders2 + ders3 + ders4) / 2);
+
+	}
+
+}
