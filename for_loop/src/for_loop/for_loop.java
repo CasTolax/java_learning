feb04f89a2fe1d6020084ce35774b1a5be4ea140
CasTolax <cantuncer32@gmail.com>
2025-07-23 12:18:31 +0300

    Add files via upload

---
+++ for_loop/src/for_loop/for_loop.java
@@ -0,0 +1,46 @@
+package for_loop;
+
+public class for_loop {
+
+	public static void main(String[] args) {
+		
+         /*for(int i = 1; i<100; i++)
+         {
+        	 if(i%2==0)
+        	 {
+        		 System.out.println(i);
+        	 }
+         }*/
+         
+         
+         
+         /*for(int i = 1; i<100; i++)
+         {
+        	 if(i%1==0)
+        	 {
+        		 System.out.println(i);
+        	 }
+         }*/
+		
+		
+		/*for(int i = 1; i<100; i++)
+        {
+       	 if(i%2==1)
+       	 {
+       		 System.out.println(i);
+       	 }
+        }*/
+		
+		
+		for (int i = 1; i<=100; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("can");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
